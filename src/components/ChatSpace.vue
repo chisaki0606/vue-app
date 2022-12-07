041d8fc2cd32ae9202af6249867e4fdc559f42f0
chisaki0606 <witness6961@gmail.com>
2022-12-07 23:03:18 +0900

    creat ChatSpace

---
+++ src/components/ChatSpace.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+
+const textInput = ref('');
+</script>
+
+
+<template>
+  <div>
+    <h2>ChatSpace</h2>
+    <div>
+      <input type="text" v-model="textInput">
+      <p>{{ textInput }}</p>
+    </div>
+  </div>
+</template>
