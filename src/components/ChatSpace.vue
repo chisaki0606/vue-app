d8dbed3313a7672cb51c88de51927b0c6b877c4a
chisaki0606 <witness6961@gmail.com>
2022-12-08 22:19:05 +0900

    add function comment add remove

--- src/components/ChatSpace.vue
+++ src/components/ChatSpace.vue
@@ -1,16 +1,30 @@
-<script setup>
-import { ref } from 'vue';
-
-const textInput = ref('');
+<script>
+export default {
+  data() {
+    return {
+      comment: '',
+      comments:[]
+    }
+  },
+  methods: {
+    addComment() {
+      this.comments.push(this.comment)
+      this.comment = ''
+    },
+    removeComment(index) {
+      this.comments.splice(index, 1)
+    }
+  }
+}
 </script>
 
-
 <template>
-  <div>
+  <div id="app">
     <h2>ChatSpace</h2>
-    <div>
-      <input type="text" v-model="textInput">
-      <p>{{ textInput }}</p>
-    </div>
+    <input v-model="comment">
+    <button @click="addComment">追加</button>
+    <ul>
+      <li v-for="(comment, index) in comments" :key="index">{{ comment }} <span @click="removeComment(index)">X</span></li>
+    </ul>
   </div>
 </template>
